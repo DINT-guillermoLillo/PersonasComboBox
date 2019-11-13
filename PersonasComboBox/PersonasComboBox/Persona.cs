ed2425b7d2e582332c4da266b49bdd8a51fa297e
alumno <alumno@DESKTOP-TJSF3MI>
2019-11-13 13:55:46 +0100

    Primer commit

---
+++ PersonasComboBox/PersonasComboBox/Persona.cs
@@ -0,0 +1,26 @@
+﻿public class Persona
+{ 
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public string Foto { get; set; }
+    public string Sexo { get; set; }
+
+    public Persona(int id, string nombre, string foto, string sexo)
+    {
+        _id = id;
+        _nombre = nombre;
+        _foto = foto;
+        _sexo = sexo;
+    }
+
+    public static ObservableCollection<Persona> GetPersonas()
+    {
+        ObservableCollection<Persona> lista = new ObservableCollection<Persona>();
+
+        lista.Add(new Persona(1, "Pepe", "1.jpg", "Hombre"));
+        lista.Add(new Persona(2, "Antonio", "2.jpg", "Hombre"));
+        lista.Add(new Persona(3, "Sara", "3.jpg", "Mujer"));
+
+        return lista;
+    }
+}
