646d68f440feb2c9f059b66467dbff293f844790
guillermoLillo <guillermo.lillo@iesdoctorbalmis.com>
2019-11-13 20:50:15 +0100

    Acabado :)

---
+++ PersonasComboBox/PersonasComboBox/BooleanConverter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace PersonasComboBox
+{
+    public class BooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch (value.ToString().ToLower())
+            {
+                case "mujer":
+                    return "Recursos\\mujer.png";
+                case "hombre":
+                    return "Recursos\\hombre.png";
+            }
+
+            return "error";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value.ToString().Contains("mujer.png"))
+                return "mujer";
+            else
+                return "hombre";
+
+
+        }
+    }
+}
--- PersonasComboBox/PersonasComboBox/Persona.cs
+++ PersonasComboBox/PersonasComboBox/Persona.cs
@@ -19,9 +19,9 @@ public class Persona
     {
         ObservableCollection<Persona> lista = new ObservableCollection<Persona>();
 
-        lista.Add(new Persona(1, "Pepe", "1.jpg", "Hombre"));
-        lista.Add(new Persona(2, "Antonio", "2.jpg", "Hombre"));
-        lista.Add(new Persona(3, "Sara", "3.jpg", "Mujer"));
+        lista.Add(new Persona(1, "Pepe", "Recursos\\1.jpg", "Hombre"));
+        lista.Add(new Persona(2, "Antonio", "Recursos\\2.jpg", "Hombre"));
+        lista.Add(new Persona(3, "Sara", "Recursos\\3.jpg", "Mujer"));
 
         return lista;
     }
